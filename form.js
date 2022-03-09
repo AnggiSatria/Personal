3f5cd0c2f867f504c4915c4c23cd5360a039318c
AnggiSatria <anggisatria122@gmail.com>
2022-03-09 09:04:23 +0700

    add 2

--- form.js
+++ form.js
@@ -1,34 +1,62 @@
-let blogs = []
+let projects = []
 
-function addBlog(){
+function addProject(){
 
-    let Name = document.getElementById('Name').value
-    let Date = document.getElementById('Date').value
-    let Date2 = document.getElementById('Date2').value
-    let Desc = document.getElementById('Desc').value
-    let Tech = document.getElementById('Tech').value
-    let Image = document.getElementById('Image').files[0]
+    let name = document.getElementById('Name').value
+    let date = document.getElementById('Date').value
+    let date2 = document.getElementById('Date2').value
+    let desc = document.getElementById('Desc').value
+    let image = document.getElementById('Image').files[0]
 
-    Image = URL.createObjectURL(Image)
+    let nodeJs = document.getElementById('nodeJs').checked
+    let reactJs = document.getElementById('reactJs').checked
+    let nextJs = document.getElementById('nextJs').checked
+    let typesScript = document.getElementById('typeScript').checked
 
-    let blog = {
-        name: Name,
-        date: Date,
-        date2: Date2,
-        desc: Desc,
-        tech: Tech,
-        image: Image,
+    image = URL.createObjectURL(image)
+
+    let tech = ""
+
+     if(nodeJs == true){
+        tech +='<img src ="image/node2.jpg"/>'
     }
 
-    blogs.push(blog)
+    if(reactJs == true){
+        tech +='<img src ="image/react-removebg-preview.png">'
+    } 
 
-    renderBlog()
+    if(nextJs == true){
+        tech +='<img src ="image/nextJs-removebg-preview.png">'
+    }
+
+    if(typesScript == true){
+        tech +='<img src ="image/ts-removebg-preview.png">'
+    }
+
+    let project = {
+        name: name,
+        date: date,
+        date2: date2,
+        desc: desc,
+        nodeJs: nodeJs,
+        reactJs: reactJs,
+        nextJs: nextJs,
+        typesScript: typesScript, 
+        image: image
+    }
+
+   
+
+
+    projects.push(project)
+
+    renderProject()
 }
 
-function renderBlog(){
-    let blogContainer = document.getElementById('Card')
+function renderProject(){
+    let projectContainer = document.getElementById('Card')
 
-    blogContainer.innerHTML = `<div class="card">
+    projectContainer.innerHTML = `<div class="card">
     <div class="img">
         <img src="assets/blog-img.png" alt="">
     </div>
@@ -75,39 +103,39 @@ function renderBlog(){
 </div>
 `
 
-    for( i = 0; i < blogs.length; i++ ){
-        blogContainer.innerHTML += `<div class="card">
+    for( i = 0; i < projects.length; i++ ){
+        projectContainer.innerHTML += `<div class="card">
         <div class="img">
-            <img src="${blogs[i].image}" alt="">
+            <img src="${projects[i].image}" alt="">
         </div>
 
         <div class="tittle">
-            <h2>${blogs[i].name}</h2>
+            <h2>${projects[i].name}</h2>
         </div>
 
         <div class="duration">
-            <h3>${blogs[i].date} - ${blogs[i].date2} </h3>
+            <h3>${getDistanceTime()}</h3>
         </div>
 
         <div class="card-content">
-            <p>${blogs[i].desc}</p>
+            <p>${projects[i].desc}</p>
         </div>
 
         <div class="after-content">
             <div class="ps">
-                <img src="assets/ps.png" alt="" style="width: 50px;">
+                <img src="${projects[i].nodeJs}" alt="" style="width: 50px;">
             </div>
 
             <div class="andro">
-                <img src="image/android.png" alt="" style="width: 50px;">
+                <img src="${projects[i].reactJs}" alt="" style="width: 50px;">
             </div>
 
             <div class="java">
-                <img src="image/java2.jpg" alt="" style="width: 50px;">
+                <img src="${projects[i].nextJs}" alt="" style="width: 50px;">
             </div>
 
             <div class="nodejs">
-                <img src="image/git.png" alt="" style="width: 50px;">
+                <img src="${projects[i].typesScript}" alt="" style="width: 50px;">
             </div>
         </div>
 
@@ -139,24 +167,66 @@ let month = [
     'October',
     'November',
     'December'
-
 ]
 
-function getFullTime(){
-    let time = new Date()
-
+function getFullTime(time){
+    
+    
+    
     let date = time.getDate()
     let monthIndex = time.getMonth()
     let year = time.getFullYear()
-
+    let hours = time.getHours()
     let minutes = time.getMinutes()
 
+    let fullTime = `${date} ${month[monthIndex]} ${year} ${hours}:${minutes} WIB`
 
-    comsole.log(date)
-    console.log(month[monthIndex])
-    console.log(year)
-    console.log(minutes)
-    console.log()
+    return fullTime
 }
 
-getFullTime()
+function getDistanceTime(){
+
+    let startTime = projects[i].date
+    console.log(startTime)
+    let endTime = projects[i].date2
+    console.log(endTime)
+
+    // let timeNow = new Date()
+    // let timeBlog = new Date(time)
+
+    // console.log('Now: ', timeNow)  
+    // console.log('Blog: ', timeBlog)
+
+    let distance = new Date(endTime) - new Date(startTime) // milliseconds 
+    console.log(distance)
+
+    let dayDistance = Math.floor(distance/( 23 * 60 * 60 * 1000 )) // convert to day 
+
+    if(dayDistance != 0){
+        return dayDistance + ' Days Ago'
+    }else{
+        let hourDistance = Math.floor(distance/( 60 * 60 * 1000 ))
+
+        if(hourDistance != 0){
+            return hourDistance + ' Hours Ago'
+        }else{
+            let minuteDistance = Math.floor(distance/(60 * 1000))
+
+            if(minuteDistance != 0){
+                 return minuteDistance + ' Minutes Ago'
+            }else{
+                let secondDistance = Math.floor(distance/1000)
+                return secondDistance + ' Second Ago'
+            }
+           
+        }
+        
+    }
+    
+}
+
+setInterval(function(){
+    renderProject()
+}, 1000)
+
+
